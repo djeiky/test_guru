e2cbb654475860ed2247ca9e783c56618f558cb7
djeik <djeik@inbox.ru>
2019-02-28 14:23:51 +0300

    fourth task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_tests
   has_many :tests, through: :user_tests
+  has_many :author, class_name: "Test", foreign_key: :author_id
 
   def tests_by_level(level)
     self.tests.where(level: level)
