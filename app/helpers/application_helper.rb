b5dfeb3d64ddd6722fde21558ad3a1d16b94c99f
djeik <djeik@inbox.ru>
2019-03-05 15:44:14 +0300

    finish seventh task

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_year
+    Date.today.year
+  end
+
+  def github_url(name, repo)
+    link_to name, "https://github.com/#{name}/#{repo}"
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,5 @@
 module QuestionsHelper
+  def question_header(question)
+    "#{question.new_record? ? 'Create New': 'Edit'} #{question.test.title} Question"
+  end
 end
