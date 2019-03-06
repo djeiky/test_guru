67819d77ead0415fdd295271936f89e00df32df9
djeik <djeik@inbox.ru>
2019-03-06 14:39:42 +0300

    correct date to use app settings, add open links in new tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def current_year
-    Date.today.year
+    Date.current.year
   end
 
   def github_url(name, repo)
-    link_to name, "https://github.com/#{name}/#{repo}"
+    link_to name, "https://github.com/#{name}/#{repo}", target: :_blank
   end
 end
