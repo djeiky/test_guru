0717717bc21e4de2bf98ea477ecd02cdec3de12d
djeik <djeik@inbox.ru>
2019-03-06 16:15:45 +0300

    add Answer controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'tests#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 end
