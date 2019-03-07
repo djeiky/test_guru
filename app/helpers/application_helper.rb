1be871dcfd557052d4260a59642c4ba27d385aa7
djeik <djeik@inbox.ru>
2019-03-08 00:14:20 +0300

    finish ninth tast

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def github_url(name, repo)
     link_to name, "https://github.com/#{name}/#{repo}", target: :_blank
   end
+
+  def flash_messages
+    flash.map do |key, value|
+      content_tag :p, value, class: "flush-#{@key}"
+    end.join.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'tests#index'
+
+  get :sign_up, to: "users#new"
+  get :log_in, to: "sessions#new"
+  get :log_out, to: "sessions#destroy"
+
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tests do
     resources :questions, shallow: true do
