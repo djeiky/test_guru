8a0f9a62ce4afe17f2924522343aac68e708bf57
djeik <djeik@inbox.ru>
2019-03-22 21:07:58 +0300

    add setup_mail.rb without not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,32 @@
   Category.find_or_create_by(title: title)
 end
 
-admin = Admin.where(email: "djeiky@gmail.com").first
-unless admin
-  admin = Admin.new
-  admin.email = "djeiky@gmail.com"
-  admin.first_name = "E"
-  admin.last_name = "A"
-  admin.password = "test123"
-  admin.password_confirmation = "test123"
-  admin.save
+@admin = Admin.where(email: "djeiky@gmail.com").first
+unless @admin
+  @admin = Admin.new
+  @admin.email = "djeiky@gmail.com"
+  @admin.first_name = "E"
+  @admin.last_name = "A"
+  @admin.password = "test123"
+  @admin.password_confirmation = "test123"
+  @admin.save
 end
-puts "admin ---------- #{admin.id}"
+puts "admin ---------- #{@admin.id}"
 
-test = Test.find_or_create_by(title: "Rails intro") do |t|
-  t.level = 1, 
-  t.category_id = Category.find_by(title: "Rails").id,
-  t.author_id = admin.id
+@test = Test.find_or_create_by(title: "Rails intro") do |t|
+  t.level = 1
+  t.category_id = Category.find_by(title: "Rails").id
+  t.author_id = @admin.id
 end
-question = Question.find_or_create_by(body: "What is Rails?") do |q|
-  q.test_id = test.id
+@question = Question.find_or_create_by(body: "What is Rails?") do |q|
+  q.test_id = @test.id
 end
 Answer.find_or_create_by(body: "Framework") do |a|
-  a.correct = true,
-  a.question_id = question.id
+  a.correct = true
+  a.question_id = @question.id
 end
 Answer.create(body: "Programm language") do |a|
-  a.correct = false,
-  a.question_id = question.id
+  a.correct = false
+  a.question_id = @question.id
 end
 
